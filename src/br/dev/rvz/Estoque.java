bfa3b5f4530d33a14ae16efbe621094fa5044063
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-05 15:18:28 -0300

    implemntado estoqeu e adicionar produto refatorado

---
+++ src/br/dev/rvz/Estoque.java
@@ -0,0 +1,19 @@
+package br.dev.rvz;
+
+public class Estoque {
+
+    private ListaProdutos lista;
+
+    public Estoque() {
+        lista = new ListaProdutos();
+    }
+
+    public void adicionarProduto(String nome, String dataVencimento, double preco, int quantidadeEmEstoque) {
+        Produto produto = new Produto();
+        produto.setNome(nome);
+        produto.setDataVencimento(dataVencimento);
+        produto.setPreco(preco);
+        produto.setQuantidadeEmEstoque(quantidadeEmEstoque);
+        lista.adicionarProduto(produto);
+    }
+}
--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -4,22 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         boolean run = true;
-        ListaProdutos listaProdutos = new ListaProdutos();
+        Estoque estoque = new Estoque();
         int option;
         IO.out("Controle de estoque - Lista de supermercados");
 
         while(run) {
             IO.out("1. Adicionar um produto\n2. Pesquisar um produto por nome\n3. Sair do programa.");
             option = IO.input().nextInt();
-
             if (option == 1) {
-                Produto produto = new Produto();
                 IO.out("Digite o nome, data de vencimento, preço e quantidade em estoque do produto!");
-                produto.setNome(IO.input().nextLine());
-                produto.setDataVencimento(IO.input().nextLine());
-                produto.setPreco(IO.input().nextDouble());
-                produto.setQuantidadeEmEstoque(IO.input().nextInt());
-                listaProdutos.adicionarProduto(produto);
+                estoque.adicionarProduto(IO.input().nextLine(), IO.input().nextLine(), IO.input().nextDouble(),IO.input().nextInt());
                 IO.out("Produto adicionado!");
             } else if (option == 2) {
                 IO.out("Digite o noem do produto:");
