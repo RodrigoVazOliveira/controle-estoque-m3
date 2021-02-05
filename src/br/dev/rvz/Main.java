6185e69c5491dd1b0b3cac838bc701235cefaad8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-05 15:37:05 -0300

    refatorado o menu inicial

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -3,27 +3,6 @@ package br.dev.rvz;
 public class Main {
 
     public static void main(String[] args) {
-        boolean run = true;
-        Estoque estoque = new Estoque();
-        int option;
-        IO.out("Controle de estoque - Lista de supermercados");
-
-        while(run) {
-            IO.out("1. Adicionar um produto\n2. Pesquisar um produto por nome\n3. Sair do programa.");
-            option = IO.input().nextInt();
-            if (option == 1) {
-                IO.out("Digite o nome, data de vencimento, preço e quantidade em estoque do produto!");
-                estoque.adicionarProduto(IO.input().nextLine(), IO.input().nextLine(), IO.input().nextDouble(),IO.input().nextInt());
-                IO.out("Produto adicionado!");
-            } else if (option == 2) {
-                IO.out("Digite o noem do produto:");
-                estoque.pesquisarProduto(IO.input().nextLine());
-            } else if (option == 3) {
-                run = false;
-            } else {
-                IO.out("oção inválida");
-            }
-        }
-
+        MainMenu mainMenu = new MainMenu();
     }
 }
---
+++ src/br/dev/rvz/MainMenu.java
@@ -0,0 +1,45 @@
+package br.dev.rvz;
+
+public class MainMenu {
+    private boolean run = true;
+    private int option;
+    private Estoque estoque;
+
+    public MainMenu() {
+        IO.out("Controle de estoque - Lista de supermercados");
+        estoque = new Estoque();
+
+        while(run) {
+            IO.out("1. Adicionar um produto\n2. Pesquisar um produto por nome\n3. Sair do programa.");
+            option = IO.input().nextInt();
+            menuOptions();
+        }
+    }
+
+    private void adicionarProduto() {
+        IO.out("Digite o nome, data de vencimento, preço e quantidade em estoque do produto!");
+        estoque.adicionarProduto(IO.input().nextLine(), IO.input().nextLine(), IO.input().nextDouble(),IO.input().nextInt());
+        IO.out("Produto adicionado!");
+    }
+
+    private void pesquisarProduto() {
+        IO.out("Digite o noem do produto:");
+        estoque.pesquisarProduto(IO.input().nextLine());
+    }
+
+    private void sair() {
+        run = false;
+    }
+
+    private void menuOptions() {
+        if (option == 1) {
+            adicionarProduto();
+        } else if (option == 2) {
+            pesquisarProduto();
+        } else if (option == 3) {
+            sair();
+        } else {
+            IO.out("oção inválida");
+        }
+    }
+}
